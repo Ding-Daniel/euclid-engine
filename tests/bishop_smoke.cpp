bef064601db4b4fba57639e8d15a10b8cd6e5758
Daniel Ding <you@example.com>
2025-09-27 17:11:58 -0700

    fixing error for legality move counts for kng/bshp

--- tests/bishop_smoke.cpp
+++ tests/bishop_smoke.cpp
@@ -6,13 +6,11 @@
 int main() {
   using namespace euclid;
 
-  // White bishop on d5, kings placed so White king has 0 legal moves
-  // (adjacent-to-black constraint blocks Ka1 moves). Expect 12 bishop moves.
-  // FEN ranks: 8/7/6/5/4/3/2/1  (top to bottom)
-  //             8/8/8/3B4/8/8/1k6/K7 w - - 0 1
+  // White bishop on d5; kings placed so White king has 0 moves.
+  // Bishop has 13 legal moves on an open board from d5.
   Board b;
   set_from_fen(b, "8/8/8/3B4/8/8/1k6/K7 w - - 0 1");
-  assert(perft(b, 1) == 12ULL);
+  assert(perft(b, 1) == 13ULL);
 
   return 0;
 }
